069cf0d142c03ac3e4a99263da929cbb60c8b501
Abhitomar01 <tabhi1957@gmail.com>
2024-12-06 21:56:25 +0530

    newsapp completed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
-import React, { Component } from 'react'
-import NavBar from './Components/Navbar'
-import News from './Components/News'
-export default class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar/>
-<News/>
-      </div>
-    )
-  }
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './Components/Navbar';
+import News from './Components/News';
+//import LoadingBar from 'react-top-loading-bar'
+//import { useState } from 'react';
+
+const App = () => {
+ // const [progress, setProgress] = useState(0)
+  return (
+    <div>
+      <Router>
+        <NavBar />
+
+       {/* <LoadingBar
+          color='#f11946'
+          height={3}
+          progress={progress}
+
+        />*/}
+        <Routes>
+          <Route exact path="/" element={<News  key="home" pageSize={9} country="us" category="general" />} />
+          <Route exact path="/business" element={<News key="business" pageSize={9} country="us" category="business" />} />
+          <Route exact path="/entertainment" element={<News  key="entertainment" pageSize={9} country="us" category="entertainment" />} />
+          <Route exact path="/general" element={<News  key="general" pageSize={9} country="us" category="general" />} />
+          <Route exact path="/health" element={<News  key="health" pageSize={9} country="us" category="health" />} />
+          <Route exact path="/science" element={<News  key="science" pageSize={9} country="us" category="science" />} />
+          <Route exact path="/sports" element={<News  key="sports" pageSize={9} country="us" category="sports" />} />
+          <Route exact path="/technology" element={<News  key="technology" pageSize={9} country="us" category="technology" />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
-
-
+export default App
--- src/Components/News.js
+++ src/Components/News.js
@@ -1,54 +1,90 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import NewsItems from './NewsItems'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from "react-infinite-scroll-component";
+const News = (props) => {
+  const [articles, setarticles] = useState([])
+  const [loading, setloading] = useState(true)
+  const [page, setpage] = useState(1)
+  const [totalResults, settotalResults] = useState(0)
 
-export default class News extends Component {
-  articals =  [
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
-  constructor(){
-    super();
-    this.state={
-articals: this.articals,
-loading:false
-    }
+
+  //captialize first word
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  render() {
-    return (
-      <div className="container my-3">
-        <h2>News- Top Headlines</h2>
-        <div className="row">
-          <div className="col-md-4">
-<NewsItems title="My news" description ="my desc" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg"
-newsUrl="To do"/>
-</div>
-<div className="col-md-4">
-<NewsItems title="My news" description ="my desc" imageUrl=""/>
-</div>
-<div className="col-md-4">
-<NewsItems title="My news" description ="my desc" imageUrl =""/>
-</div>
-</div>
-      </div>
-      
-    )
+
+  //refactoring//
+
+  const updateNews = async () => {
+   // props.setProgress(0);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=bf71a124419a473b9faee0453b6528b3&page=${page}&pageSize=${props.pageSize}`;
+    setloading(true)
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    setarticles(parsedData.articles)
+    settotalResults(parsedData.totalResults)
+    setloading(false)
+    // props.setProgress(100);
   }
+
+  //handling and fetching API//
+  useEffect(() => {
+     document.title = `${capitalizeFirstLetter(props.category)} - Newsapp`;
+    updateNews()
+  },[])
+
+  
+  const fetchMoreData = async () => {
+
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=bf71a124419a473b9faee0453b6528b3&page=${page + 1}&pageSize=${props.pageSize}`;
+    setpage(page + 1)
+    let data = await fetch(url);
+    let parsedData = await data.json()
+    console.log(parsedData);
+    setarticles(articles.concat(parsedData.articles))
+    settotalResults(parsedData.totalResults)
+  }
+
+  return (
+    <>
+      <h1 className='text-center' style={{ margin: '35px 0px', marginTop: '90px' }}>News- Top Headlines from{capitalizeFirstLetter(props.category)}</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">         <div className="row">
+          {articles.map((element) => {
+            return <div className="col-md-4" key={element.url}>
+              <NewsItems title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage}
+                newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name
+
+                } />
+            </div>
+
+          })}
+
+        </div>
+        </div>
+
+      </InfiniteScroll>
+    </>
+
+  )
 }
+News.defaultProps = {
+  name: 'us',
+  pageSize: 9,
+  //category:general
+}
+News.propTypes = {
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string
+}
+export default News
